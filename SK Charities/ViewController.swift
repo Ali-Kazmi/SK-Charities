f752326386168f5cc0f1584c87258ea9a435e48f
ali <3ali.kazmi@gmail.com>
2019-04-21 15:07:01 -0700

    Initial Commit

---
+++ SK Charities/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SK Charities
+//
+//  Created by ali on 4/21/19.
+//  Copyright © 2019 ali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SK CharitiesTests/SK_CharitiesTests.swift
@@ -0,0 +1,34 @@
+//
+//  SK_CharitiesTests.swift
+//  SK CharitiesTests
+//
+//  Created by ali on 4/21/19.
+//  Copyright © 2019 ali. All rights reserved.
+//
+
+import XCTest
+@testable import SK_Charities
+
+class SK_CharitiesTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
